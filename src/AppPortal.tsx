61464447b4a7cc9b481312178d2763a1b2d5ba06
Marquis Ogre <marquisogre@gmail.com>
2026-09-26 11:48:56 +0530

    Use shared checkout frame context

--- src/AppPortal.tsx
+++ src/AppPortal.tsx
@@ -224,9 +224,6 @@ function CustomerPortal({
           update={update}
           remove={remove}
           students={students}
-          schoolName={schoolName}
-          branchName={branchName}
-          userLabel={studentId}
           onComplete={() => {
             setCart([])
             setCheckout('success')
